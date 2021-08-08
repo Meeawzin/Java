4b95bb52b02ca0fb6331b2fdfa9966f85f247b2c
meeawzin <fhaael_pk@hotmail.com>
2021-08-08 16:30:11 -0300

    Revisão do conteudo aprendido

--- curso_programacao/src/main.java
+++ curso_programacao/src/main.java
@@ -4,29 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world!");
-		System.out.println();
-		
-		//exercicio de fixação 1
-		
-		int q = 10;
-		
-		System.out.println("RESULTADO = " + q + " metros");
-		System.out.println();
-
-		
-		//exercicio de fixação 2
-		
-		String name = "Maria";
-		int idad = 25;
-		double pay = 4000.0;
-		
-		System.out.printf("%s tem %d anos e recebe $ %.2f%n", name , idad , pay);
-		System.out.println();
-
-		
-		//exercicio de fixação 3
-		
+		//fixação 1
 		
 		String product1 = "Computer";
 		String product2 = "Office desk";
@@ -35,15 +13,15 @@ public class main {
 		int code = 5290;
 		char gender = 'F';
 		
-		double price1 = 2100.00;
+		double price1 = 2100.0;
 		double price2 = 650.50;
-		double measure = 52.234567;
+		double measure = 53.234567;
 		
 		System.out.println("Products:");
 		System.out.printf("%s, which price is $ %.2f%n", product1, price1);
 		System.out.printf("%s, which price is $ %.2f%n", product2, price2);
 		System.out.println();
-		System.out.printf("Record: %d years old, code %d and gender: %c%n", age , code , gender);
+		System.out.printf("Record: %d years old, code %d and gender: %c%n", age, code, gender);
 		System.out.println();
 		System.out.printf("Measue with eight decimal places: %.8f%n", measure);
 		System.out.printf("Rouded (three decimal places): %.3f%n", measure);
@@ -51,44 +29,40 @@ public class main {
 		System.out.printf("US decimal point: %.3f%n", measure);
 		System.out.println();
 
-				
-		//exercicio de fixação 4 - aritmética
+		
+		//fixação 2
 		
 		double b, B, h, area;
 		
 		b = 6;
 		B = 8;
 		h = 5;
-		area = ( b + B ) / 2.0 * h;
+		area = (b + B) / 2.0 * h;
 		
 		System.out.println(area);
+		System.out.println();
+
 		
+		//fixação casting
 		
-		//exercicio de fixação 5 - casting
-		
-		int w,e;
+		int x,y;
 		double resultado;
 		
-		w = 5;
-		e = 2;
-		resultado = (double) w/e;
+		x = 5;
+		y = 2;
+		resultado = (double) x / y;
 		
 		System.out.println(resultado);
 		
-		//exercicio de fixação 6 - casting
-		
-		int r;
-		double t;
-		
-		t = 5.0;
-		r = (int) t;
-		
-		System.out.println(r);
+			
 		
 		
 		
-	
 		
+		
+
+
+				
 		
 	
 		
