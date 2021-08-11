20299772efbb1187eff249ed8127152f6b9d970c
meeawzin <fhaael_pk@hotmail.com>
2021-08-11 13:12:33 -0300

    Revisão e novos exercicios propostos

--- sqrt/src/Main.java
+++ sqrt/src/Main.java
@@ -3,15 +3,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-	double a, b, c, delta, x1, x2;
-	
-		
-	delta = Math.pow(b, 2.0) - 4*a*c;
-	
-	x1 = (-b + Math.sqrt(delta)) / (2.0 * a);
-	x2 = (-b - Math.sqrt(delta)) / (2.0 * a);
 
-	System.out.println(delta);
+		double x1,x2, delta, a,b,c;
+		
+		a = 2;
+		b = 4;
+		c = 6;
+		
+		
+		delta = Math.pow(b, 2.0) - 4*a*c;
+		x1 = (-b + Math.sqrt(delta)) / (2.0 * a);
+		x2 = (-b - Math.sqrt(delta)) / (2.0 * a);	
+		
+		System.out.println(x1);
+		System.out.println(x2);
+
+		
+		
+		
 	}
 
 }
