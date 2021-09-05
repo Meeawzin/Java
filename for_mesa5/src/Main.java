ba21b6e90120c38349e9d41337be03c74ae0a959
meeawzin <fhaael_pk@hotmail.com>
2021-09-04 22:07:32 -0300

    Revisão exercicios de mesa no caderno, dia 04/08

--- for_mesa5/src/Main.java
+++ for_mesa5/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		// exercicio feito no caderno pela terceira vez dia 03/08
+		// exercicio feito no caderno pela quarta vez dia 04/08
 
 		int x,y,i;
 		
